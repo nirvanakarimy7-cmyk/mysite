95f9c421a1aa3b5f7e3b637ae99aaaf8deaada9f
nirvanakarimy7-cmyk <nirvanakarimy7@gmail.com>
2026-09-05 18:59:09 +0330

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-mbz8vrt4fb&ilq8ubo=pcu*qiwu41jwd7hsy7x+)$gz96!8el$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['mysite-iaue.onrender.com']
+ALLOWED_HOSTS = ['mysite-2-b2h7.onrender.com']
 
 
 # Application definition
