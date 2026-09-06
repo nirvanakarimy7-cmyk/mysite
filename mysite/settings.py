1d7db12179d5795fd72b66b6f186059d33ab11fa
nirvanakarimy7-cmyk <nirvanakarimy7@gmail.com>
2026-09-06 16:49:07 +0330

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -66,7 +66,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        default=,
         conn_max_age=600
     )
 }
