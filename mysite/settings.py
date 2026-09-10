ff5e2ba61b688b8e3d4c0a84e28d8ea1bc7e9fbd
Mobina <nirvanakarimy7@gmail.com>
2026-09-10 22:55:00 +0330

    Use CloudinaryField for product images

--- mysite/settings.py
+++ mysite/settings.py
@@ -117,8 +117,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
-
-
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
@@ -126,13 +124,9 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
--- mysite/shop/models.py
+++ mysite/shop/models.py
@@ -3,6 +3,7 @@ from django.core.validators import MinValueValidator,MaxValueValidator
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 import datetime
+from cloudinary.models import CloudinaryField
 
 class Category(models.Model):
     name=models.CharField(max_length=20)
@@ -47,7 +48,7 @@ class Product(models.Model):
     description=models.CharField(max_length=500,default='', blank=True , null=True)
     price=models.DecimalField(default=0, decimal_places=0, max_digits=12)
     Category = models.ForeignKey(Category ,on_delete=models.CASCADE ,default=1)
-    picture = models.ImageField(upload_to='upload/product/')
+    picture =picture = CloudinaryField('image')
     star=models.IntegerField(default=0,validators=[MaxValueValidator(5),MinValueValidator(0)])
     is_sale=models.BooleanField(default=False)
     sale_price=models.DecimalField(default=0, decimal_places=0, max_digits=12)
