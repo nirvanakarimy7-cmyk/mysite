3d2efaf627e550bd7e6e207199e86b7a11757eda
Mobina <nirvanakarimy7@gmail.com>
2026-09-12 21:33:52 +0330

    Configure Cloudinary

--- mysite/settings.py
+++ mysite/settings.py
@@ -135,3 +135,12 @@ CLOUDINARY_STORAGE = {
 }
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+import cloudinary
+
+cloudinary.config(
+    cloud_name=os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    api_key=os.environ.get('CLOUDINARY_API_KEY'),
+    api_secret=os.environ.get('CLOUDINARY_API_SECRET'),
+    secure=True
+)
