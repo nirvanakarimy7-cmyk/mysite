9675dcdbe2676582329d1117629c8aebe77fed0d
Mobina <nirvanakarimy7@gmail.com>
2026-09-19 16:58:19 +0330

    Fix build script

--- mysite/build.sh
+++ mysite/build.sh
@@ -1,7 +1,5 @@
 set -o errexit
 pip install -r requirements.txt
-<<<<<<< HEAD
 python manage.py migrate
-=======
->>>>>>> 9e06aa39a9d1918b4e1bcb3e31f03b2c3b39c2b9
+python manage.py createsuperuser --noinput
 python manage.py collectstatic --noinput
